71aa106fe4dbf465808b5e6a0080b7a90cd61a2b
Julia <130411614+Julyova@users.noreply.github.com>
2023-05-27 01:29:35 +0300

    Add files via upload

---
+++ Project JQuery/js/main.js
@@ -0,0 +1,26 @@
+﻿
+const overlay = $('#overlay');
+const open_modal = $('.open_modal');
+const close = $('.modal_close, #overlay');
+const modal = $('.modal_div');
+
+open_modal.click(function (event) {
+    event.preventDefault();
+    const div = $(this).attr('href');
+    overlay.fadeIn(400,
+        function () {
+            $(div)
+                .css('display', 'block')
+                .animate({ opacity: 1, top: '50%' }, 200);
+        });
+});
+
+close.click(function () {
+    modal
+        .animate({ opacity: 0, top: '45%' }, 200,
+            function () {
+                $(this).css('display', 'none');
+                overlay.fadeOut(400);
+            }
+        );
+});
